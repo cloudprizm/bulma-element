540e502b95e1c690807ccb06fa8588625d5ecb64
Damian Baar <damian.baar@gmail.com>
2019-01-09 16:24:55 +0100

    bulma-theme: adding typings for bridge

--- stories/bridge.tsx
+++ stories/bridge.tsx
@@ -5,11 +5,16 @@ import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs } from '@storybook/addon-knobs'
 
-export const makeStory = module => ({
+export const makeStory = (module: NodeModule) => ({
   component: WrapBy,
   namespace,
   name,
   props: { children }
+}: {
+  component: React.ComponentType<unknown>,
+  namespace: string,
+  name: string,
+  props: { children: () => React.ReactChildren | React.ReactElement<unknown> }
 }) =>
   storiesOf(namespace, module)
     .addDecorator(withKnobs)
